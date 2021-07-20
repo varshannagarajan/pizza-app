1a34de17d17f6a7f4fc7cc3949ed78f47091f8d9
Varshan Nagarajan <48139461+varshannagarajan@users.noreply.github.com>
2021-07-19 22:33:55 -0400

    Select Topping, Size and Quantity

--- src/app/mainpage/mainpage.component.ts
+++ src/app/mainpage/mainpage.component.ts
@@ -8,22 +8,48 @@ import { Topping } from '../topping.model';
   styleUrls: ['./mainpage.component.scss'],
 })
 export class MainpageComponent implements OnInit {
-  toppings: Topping[] = [new Topping('Vegetable', 5.99),
-  new Topping('Hawaiian', 5.99),
-  new Topping('Margherita', 4.99),
-  new Topping('Extra Cheese', 3.99)];
+  numPizzas: string;
+  toppings: Topping[] = [
+    new Topping('Vegetable', 5.99),
+    new Topping('Hawaiian', 5.99),
+    new Topping('Margherita', 4.99),
+    new Topping('Extra Cheese', 3.99)];
 
-  sizes: Size[] = [new Size('Small', 10.99),
-  new Size('Medium', 11.99),
-  new Size('Large', 12.99),
-  new Size('Party', 15.99)];
+  sizes: Size[] = [
+    new Size('Small', 10.99),
+    new Size('Medium', 11.99),
+    new Size('Large', 12.99),
+    new Size('Party', 15.99)];
 
+  selectedTopping: Topping;
+  selectedSize: Size;
   constructor() {
-
+    this.numPizzas = '0';
   }
 
   ngOnInit() {
 
   }
+  appendDigit(digit: string) {
+    this.numPizzas = this.numPizzas == '0' ? digit : this.numPizzas + digit;
+    console.log(this.numPizzas)
+  }
 
+  reset() {
+    this.numPizzas = '0';
+  }
+
+  buy() {
+
+  }
+
+  selectTopping(topping: Topping) {
+    this.selectedTopping = topping;
+    console.log(this.selectedTopping)
+  }
+
+  selectSize(size: Size) {
+    this.selectedSize = size;
+    console.log(this.selectedSize)
+  }
 }
