a9e192ea45076c24e497171329a7d3de1e797ec1
Varshan Nagarajan <48139461+varshannagarajan@users.noreply.github.com>
2021-07-23 02:30:07 -0400

    Completed New Order Button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { MainpageComponent } from './mainpage/mainpage.component';
+import { ManagerService } from './manager.service';
+import { Order } from './order.model';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -10,7 +12,10 @@ export class AppComponent {
     { title: 'Current Order', url: '/currentorder', icon: 'bag-check' },
     { title: 'Update Order', url: '/mainpage', icon: 'create' },
     { title: 'Previous Orders', url: '/previousorders', icon: 'albums' },
-    { title: 'New Order', url: '', icon: 'refresh' },
   ];
-  constructor() { }
+  constructor(private ms: ManagerService) { }
+
+  newOrder() {
+    this.ms.currentOrder = new Order([], new Date(), 0, 0)
+  }
 }
